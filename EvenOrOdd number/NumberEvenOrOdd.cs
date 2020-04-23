2604e865101f2ff428e1964fd2a033dbf29a7c33
Justin Peel <justinpeel25@gmail.com>
2020-04-23 12:04:30 +0200

    Initial Commit

---
+++ EvenOrOdd number/NumberEvenOrOdd.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace EvenOrOdd_number
+{
+	[TestFixture]
+	public class NumberEvenOrOdd
+	{
+		[TestCase("Even", 2)]//arrange
+		[TestCase("Even", 0)]
+		public void GivenNumber_NumberIsEven_ShouldReturnEven(string expected, int number)
+		{
+			//act
+			var actual = EvenOrOdd_Number(number);
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestCase("Odd", 1)]//arrange
+		[TestCase("Odd", 7)]
+		[TestCase("Odd",-1)]
+		public void GivenNumber_NumberIsOdd_ShouldReturnOdd(string expected, int number)
+		{
+			//act
+			var actual = EvenOrOdd_Number(number);
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		private string EvenOrOdd_Number(int number)
+		{
+			/*if (number % 2 == 0)
+				return "Even";
+			return "Odd";*/
+			return number % 2 == 0 ? "Even" : "Odd";
+		}
+	}
+}
---
+++ EvenOrOdd number/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EvenOrOdd number")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EvenOrOdd number")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bda3411-4f05-4b60-beb9-20d833e5c104")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
